fc691453352ec0db33f281292b8a47736a1a7e46
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2025-04-22 20:15:58 +0300

    Refactor device monitor

--- device/src/timed_monitor/mod.rs
+++ device/src/timed_monitor/mod.rs
@@ -10,7 +10,10 @@ use arduino_hal::{
     },
 };
 use avr_device::interrupt;
-use monitor::{MonitorButtons, MonitorState};
+use monitor::{
+    BacklightMonitor, LedButtonMonitor, MonitorState, PowerButtonMonitor, SpeedDownButtonMonitor,
+    SpeedUpButtonMonitor,
+};
 use pins::{
     BacklightMonitorPin, LedMonitorPin, PowerMonitorPin, SpeedDownMonitorPin, SpeedUpMonitorPin,
 };
@@ -47,33 +50,143 @@ pub fn setup_timed_monitor(
 
     // Initialize the timer context.
     MONITOR_CTX.init(MonitorContext::new(
-        MonitorButtons::new(
-            speed_up_mon_pin,
-            speed_down_mon_pin,
-            power_mon_pin,
-            led_mon_pin,
-        ),
-        MonitorState::new(backlight_mon_pin),
+        SpeedUpButtonMonitor::new(speed_up_mon_pin),
+        SpeedDownButtonMonitor::new(speed_down_mon_pin),
+        PowerButtonMonitor::new(power_mon_pin),
+        LedButtonMonitor::new(led_mon_pin),
+        BacklightMonitor::new(backlight_mon_pin),
     ));
 }
 
 /// Contains components used exclusively in the timer interrupt.
 struct MonitorContext {
-    buttons: MonitorButtons,
-    state: MonitorState,
+    speed_up_monitor: SpeedUpButtonMonitor,
+    speed_down_monitor: SpeedDownButtonMonitor,
+    power_monitor: PowerButtonMonitor,
+    led_monitor: LedButtonMonitor,
+    backlight_monitor: BacklightMonitor,
+    monitor_state: MonitorState,
+    buttons_state: u64,
+    buttons_history: u8,
 }
 
 impl MonitorContext {
     #[inline]
-    fn new(buttons: MonitorButtons, state: MonitorState) -> Self {
-        Self { buttons, state }
+    fn new(
+        speed_up_monitor: SpeedUpButtonMonitor,
+        speed_down_monitor: SpeedDownButtonMonitor,
+        power_monitor: PowerButtonMonitor,
+        led_monitor: LedButtonMonitor,
+        backlight_monitor: BacklightMonitor,
+    ) -> Self {
+        Self {
+            speed_up_monitor,
+            speed_down_monitor,
+            power_monitor,
+            led_monitor,
+            backlight_monitor,
+            monitor_state: MonitorState::Active,
+            buttons_state: 0,
+            buttons_history: 0,
+        }
     }
 
     #[inline]
     fn monitor(&mut self) {
         interrupt::free(|cs| {
             let shared_state = &mut SHARED_STATE.borrow(cs).borrow_mut();
-            self.buttons.monitor(shared_state, &mut self.state);
+
+            let speed_up_pressed = self.speed_up_monitor.is_pressed();
+            let speed_down_pressed = self.speed_down_monitor.is_pressed();
+            let power_pressed = self.power_monitor.is_pressed();
+            let led_pressed = self.led_monitor.is_pressed();
+
+            let backlight_active = self.backlight_monitor.is_active();
+
+            let any_button_pressed =
+                speed_up_pressed || speed_down_pressed || power_pressed || led_pressed;
+
+            match self.monitor_state {
+                MonitorState::Active => {
+                    self.buttons_state = (self.buttons_state << 1) ^ u64::from(any_button_pressed);
+
+                    // A button short press requires the state to be low for 40ms. We therefore
+                    // look for a sequence of a 0 bit followed by 40 `1` bits and handle that
+                    // depending on the button priority and which ones are pressed.
+                    if self.buttons_state << 23 == 0x7FFF_FFFF_FF80_0000 {
+                        if speed_up_pressed {
+                            self.monitor_state = MonitorState::Paused;
+
+                            if backlight_active {
+                                shared_state.send_state = true;
+                                shared_state.device_state.fan_speed.increase();
+                            }
+                        } else if speed_down_pressed {
+                            self.monitor_state = MonitorState::Paused;
+
+                            if backlight_active {
+                                shared_state.send_state = true;
+                                shared_state.device_state.fan_speed.decrease();
+                            }
+                        } else if power_pressed {
+                            self.monitor_state = MonitorState::PowerFocused;
+                        } else if led_pressed {
+                            self.monitor_state = MonitorState::LedFocused;
+                        }
+                    }
+                }
+                MonitorState::Paused => {
+                    if !any_button_pressed {
+                        self.monitor_state = MonitorState::Active;
+                        self.buttons_history = 0;
+                        self.buttons_state = 0;
+                    }
+                }
+                MonitorState::PowerFocused => {
+                    self.buttons_state = (self.buttons_state << 1) ^ u64::from(power_pressed);
+
+                    if self.buttons_history < 21 {
+                        if self.buttons_state == u64::MAX {
+                            self.buttons_state = 0;
+                            self.buttons_history += 1;
+                        }
+                        // Short press triggered
+                        else if !power_pressed {
+                            self.monitor_state = MonitorState::Paused;
+
+                            shared_state.send_state = true;
+                            shared_state.device_state.power_enabled =
+                                !shared_state.device_state.power_enabled;
+                        }
+                    }
+                    // Long press triggered
+                    else if self.buttons_state == 0x00FF_FFFF_FFFF_FFFF {
+                        self.monitor_state = MonitorState::Paused;
+                    }
+                }
+                MonitorState::LedFocused => {
+                    self.buttons_state = (self.buttons_state << 1) ^ u64::from(led_pressed);
+
+                    if self.buttons_history < 21 {
+                        if self.buttons_state == u64::MAX {
+                            self.buttons_state = 0;
+                            self.buttons_history += 1;
+                        }
+                        // Short press triggered
+                        else if !led_pressed {
+                            self.monitor_state = MonitorState::Paused;
+                        }
+                    }
+                    // Long press triggered
+                    else if self.buttons_state == 0x00FF_FFFF_FFFF_FFFF {
+                        self.monitor_state = MonitorState::Paused;
+                        shared_state.send_state = true;
+
+                        shared_state.device_state.leds_enabled =
+                            !shared_state.device_state.leds_enabled;
+                    }
+                }
+            }
         });
     }
 }
--- device/src/timed_monitor/monitor.rs
+++ device/src/timed_monitor/monitor.rs
@@ -6,20 +6,14 @@ use arduino_hal::port::{
 use super::pins::{
     BacklightMonitorPin, LedMonitorPin, PowerMonitorPin, SpeedDownMonitorPin, SpeedUpMonitorPin,
 };
-use crate::{
-    shared_state::SharedState,
-    timed_monitor::pins::{LongPressPin, ShortPressPin},
-};
 
-type ExtendedButtonMonitor<PIN> = ButtonMonitor<PIN, u8>;
+pub type SpeedUpButtonMonitor = ButtonMonitor<SpeedUpMonitorPin>;
+pub type SpeedDownButtonMonitor = ButtonMonitor<SpeedDownMonitorPin>;
+pub type PowerButtonMonitor = ButtonMonitor<PowerMonitorPin>;
+pub type LedButtonMonitor = ButtonMonitor<LedMonitorPin>;
+pub type BacklightMonitor = Monitor<BacklightMonitorPin>;
 
-type SpeedUpButtonMonitor = ButtonMonitor<SpeedUpMonitorPin>;
-type SpeedDownButtonMonitor = ButtonMonitor<SpeedDownMonitorPin>;
-type PowerButtonMonitor = ExtendedButtonMonitor<PowerMonitorPin>;
-type LedButtonMonitor = ExtendedButtonMonitor<LedMonitorPin>;
-type BacklightMonitor = Monitor<BacklightMonitorPin>;
-
-struct Monitor<PIN>(Pin<Input<PullUp>, PIN>);
+pub struct Monitor<PIN>(Pin<Input<PullUp>, PIN>);
 
 impl<PIN> Monitor<PIN>
 where
@@ -36,165 +30,26 @@ where
     }
 }
 
-struct ButtonMonitor<PIN, HIST = ()> {
-    /// Pin used
-    monitor: Monitor<PIN>,
-    /// State bits
-    state: u64,
-    /// State history
-    history: HIST,
-}
+pub struct ButtonMonitor<PIN>(Monitor<PIN>);
 
-impl<PIN, HIST> ButtonMonitor<PIN, HIST>
+impl<PIN> ButtonMonitor<PIN>
 where
-    PIN: ShortPressPin,
-    HIST: Default,
+    PIN: PinOps,
 {
     #[inline]
-    fn new(pin: Pin<Input<PullUp>, PIN>) -> Self {
-        Self {
-            monitor: Monitor::new(pin),
-            state: 0,
-            history: HIST::default(),
-        }
+    pub fn new(pin: Pin<Input<PullUp>, PIN>) -> Self {
+        Self(Monitor::new(pin))
     }
 
     #[inline]
-    fn is_pressed(&self) -> bool {
-        self.monitor.is_active()
-    }
-}
-impl<PIN, HIST> ButtonMonitor<PIN, HIST>
-where
-    Self: ButtonStateUpdate,
-    PIN: ShortPressPin,
-    HIST: Default,
-{
-    /// Monitors the button and returns whether it was pressed.
-    fn monitor(
-        &mut self,
-        shared_state: &mut SharedState,
-        monitor_state: &mut MonitorState,
-    ) -> bool {
-        let is_pressed = self.is_pressed();
-        self.state = (self.state << 1) ^ u64::from(is_pressed);
-        self.update_state(shared_state, monitor_state);
-        is_pressed
+    pub fn is_pressed(&self) -> bool {
+        self.0.is_active()
     }
 }
 
-trait ButtonStateUpdate {
-    /// Tracks and updates the button state, registering presses if necessary.
-    fn update_state(&mut self, shared_state: &mut SharedState, monitor_state: &mut MonitorState);
-}
-
-impl<PIN> ButtonStateUpdate for ButtonMonitor<PIN>
-where
-    PIN: ShortPressPin,
-{
-    #[inline]
-    fn update_state(&mut self, shared_state: &mut SharedState, monitor_state: &mut MonitorState) {
-        // If a speed button is pressed when the backlight is **NOT** active then
-        // buttons get disabled until all of them are released. The physical button
-        // press however activates the backlight, thus a subsequent press will be correctly
-        // registered.
-        if self.state & 1 == 1 && !monitor_state.backlight_monitor.is_active() {
-            monitor_state.buttons_enabled = false;
-        }
-
-        // Speed buttons short press require the pin to be low for 40ms. A button release is not
-        // necessary for the press to be registered. We therefore look for a sequence of a 0
-        // bit followed by 40 `1` bits.
-        if self.state << 23 == 0x7FFF_FFFF_FF80_0000 {
-            PIN::register_short_press(shared_state, monitor_state);
-        }
-    }
-}
-
-impl<PIN> ButtonStateUpdate for ExtendedButtonMonitor<PIN>
-where
-    PIN: LongPressPin,
-{
-    #[inline]
-    fn update_state(&mut self, shared_state: &mut SharedState, monitor_state: &mut MonitorState) {
-        // TODO: document order of operations
-        if self.history < 21 {
-            // Long press pending
-            if self.state == u64::MAX {
-                self.state = 0;
-                self.history += 1;
-            }
-            // Short press
-            else if self.state << 23 == 0xFFFF_FFFF_FF00_0000
-                || (self.history > 0 && self.state & 1 == 0)
-            {
-                self.history = 0;
-                PIN::register_short_press(shared_state, monitor_state);
-            }
-        } else {
-            // Long press released
-            if self.state << 7 == 0xFFFF_FFFF_FFFF_FF00 {
-                self.history = 0;
-            }
-            // Long press triggered
-            else if self.state == 0x00FF_FFFF_FFFF_FFFF {
-                PIN::register_long_press(shared_state, monitor_state);
-            }
-        }
-    }
-}
-
-#[allow(clippy::struct_field_names)]
-pub struct MonitorButtons {
-    speed_up_monitor: SpeedUpButtonMonitor,
-    speed_down_monitor: SpeedDownButtonMonitor,
-    power_monitor: PowerButtonMonitor,
-    led_monitor: LedButtonMonitor,
-}
-
-impl MonitorButtons {
-    #[inline]
-    pub fn new(
-        speed_up_mon_pin: Pin<Input<PullUp>, SpeedUpMonitorPin>,
-        speed_down_mon_pin: Pin<Input<PullUp>, SpeedDownMonitorPin>,
-        power_mon_pin: Pin<Input<PullUp>, PowerMonitorPin>,
-        led_mon_pin: Pin<Input<PullUp>, LedMonitorPin>,
-    ) -> Self {
-        Self {
-            speed_up_monitor: SpeedUpButtonMonitor::new(speed_up_mon_pin),
-            speed_down_monitor: SpeedDownButtonMonitor::new(speed_down_mon_pin),
-            power_monitor: PowerButtonMonitor::new(power_mon_pin),
-            led_monitor: LedButtonMonitor::new(led_mon_pin),
-        }
-    }
-
-    #[inline]
-    pub fn monitor(&mut self, shared_state: &mut SharedState, monitor_state: &mut MonitorState) {
-        // If buttons are disabled (a command was issued), check if all buttons are released.
-        // As long as a button is still pressed, no other button presses are registered.
-        let any_button_pressed = self.speed_up_monitor.monitor(shared_state, monitor_state)
-            || self.speed_down_monitor.monitor(shared_state, monitor_state)
-            || self.power_monitor.monitor(shared_state, monitor_state)
-            || self.led_monitor.monitor(shared_state, monitor_state);
-
-        // The monitor state is modified when a press completes.
-        // We only want to update it if a command was registered and
-        // buttons were disabled.
-        monitor_state.buttons_enabled |= !any_button_pressed;
-    }
-}
-
-pub struct MonitorState {
-    backlight_monitor: BacklightMonitor,
-    pub(super) buttons_enabled: bool,
-}
-
-impl MonitorState {
-    #[inline]
-    pub fn new(backlight_mon_pin: Pin<Input<PullUp>, BacklightMonitorPin>) -> Self {
-        Self {
-            backlight_monitor: BacklightMonitor::new(backlight_mon_pin),
-            buttons_enabled: true,
-        }
-    }
+pub enum MonitorState {
+    Active,
+    Paused,
+    PowerFocused,
+    LedFocused,
 }

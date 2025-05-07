2dcecccda2b0bd59e01980ffd0322b0893bb2762
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2025-05-07 22:03:49 +0300

    Refactored BacklightMonitor::is_active

--- device/src/timed_monitor/monitor.rs
+++ device/src/timed_monitor/monitor.rs
@@ -12,17 +12,28 @@ pub type SpeedDownButtonMonitor = ButtonMonitor<SpeedDownMonitorPin>;
 pub type PowerButtonMonitor = ButtonMonitor<PowerMonitorPin>;
 pub type LedButtonMonitor = ButtonMonitor<LedMonitorPin>;
 
-pub struct BacklightMonitor(Pin<Input<PullUp>, BacklightMonitorPin>);
+pub struct BacklightMonitor {
+    pin: Pin<Input<PullUp>, BacklightMonitorPin>,
+    state: bool,
+}
 
 impl BacklightMonitor {
     #[inline]
     pub fn new(pin: Pin<Input<PullUp>, BacklightMonitorPin>) -> Self {
-        Self(pin)
+        Self {
+            state: pin.is_low(),
+            pin,
+        }
     }
 
+    /// Consider both whether the backlight was previously active and whether it is currently active
+    /// to avoid the race condition where the button press activates the backlight and the read
+    /// indicates that it *is* active although it did not use to be.
     #[inline]
-    pub fn is_active(&self) -> bool {
-        self.0.is_low()
+    pub fn is_active(&mut self) -> bool {
+        let prev_state = self.state;
+        self.state = self.pin.is_low();
+        prev_state && self.state
     }
 }
 

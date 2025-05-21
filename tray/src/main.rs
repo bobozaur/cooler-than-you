3747f50380142a4c19541775265be760480b56eb
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2025-05-21 14:35:50 +0300

    tray: add packaging using cargo-deb

--- tray/src/main.rs
+++ tray/src/main.rs
@@ -1,11 +1,11 @@
 //! TODO:
-//! - debian packaging
 //! - comments and docs
-//! - ~fancy icon~
-//! 
+//! - check if things like udevadm have to be run on install
+//!
 //! System tray icon taken from: <https://www.svgrepo.com/svg/503337/fan-circled>.
 //! - 64x64px
-//! - stored in /usr/share/icons/hicolor/64x64/status/cooler-than-you-symbolic.svg, chmod 644, chown root
+//! - stored in /usr/share/icons/hicolor/64x64/status/cooler-than-you-symbolic.svg, chmod 644, chown
+//!   root
 
 use tracing_subscriber::{EnvFilter, fmt};
 use tray::{AnyResult, Device, Indicator};
